87131c8b5d1b1983c7ecbcbab55462948bf2bbc8
taubacca <tbaccarin@canvas.is>
2014-11-06 12:00:01 -0500

    First Commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// JavaScript Document
+// Written by Chris Converse
+// for Lynda.com
+
+if (typeof CANVAS=="undefined") 
+{
+	CANVAS ={};
+	
+}
+
+$(document).ready(function() 
+{	
+	trace('ready!!!');
+});
+
+$(window).load(function() 
+{
+ 	
+});
+
+
+function trace(theObject)
+{
+	console.log(theObject);
+}
